24fd8adb36debf5ede0cec4beca3dd1399ebaae1
giulioz <mail.zausa.giulio@gmail.com>
2019-06-27 13:22:15 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,108 @@
+import React, { useEffect, useMemo, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Button,
+  TextInput,
+  SafeAreaView
+} from "react-native";
+
+import Audio from "./components/Audio";
+import { playlistUrl, serverHeaders } from "./urls";
+
+function getFilename(url) {
+  const splitted = url.split("/");
+  return splitted[splitted.length - 1];
+}
+
+export default function App() {
+  const [playlist, setPlaylist] = useState([]);
+  const [position, setPosition] = useState(0);
+  const [search, setSearch] = useState("");
+  const [searchInput, setSearchInput] = useState("");
+
+  useEffect(() => {
+    async function loadPlaylist() {
+      const req = await fetch(playlistUrl, { headers: serverHeaders });
+
+      const text = await req.text();
+      const lines = text
+        .split("\n")
+        .filter(s => s.length > 0)
+        .sort();
+      setPlaylist(lines.map((l, i) => ({ url: l, index: i })));
+    }
+
+    loadPlaylist();
+  }, []);
+
+  const path = useMemo(
+    () =>
+      playlist[position] && {
+        uri: encodeURI(playlist[position].url),
+        headers: serverHeaders
+      },
+    [playlist, position]
+  );
+
+  function advance() {
+    setPosition(i => (i + 1 < playlist.length ? i + 1 : 0));
+  }
+  function prev() {
+    setPosition(i => (i - 1 >= 0 ? i - 1 : playlist.length - 1));
+  }
+
+  const filteredPL = playlist.filter(
+    e => search === "" || e.url.includes(search)
+  );
+
+  return (
+    <SafeAreaView style={styles.container}>
+      {path && path.uri ? (
+        <Audio
+          path={path}
+          next={advance}
+          prev={prev}
+          title={getFilename(playlist[position].url)}
+        />
+      ) : null}
+
+      <View style={styles.row}>
+        <Text>Search:</Text>
+        <TextInput
+          style={styles.grow}
+          onChangeText={setSearchInput}
+          value={searchInput}
+        />
+        <Button onPress={() => setSearch(searchInput)} title={"Go"} />
+      </View>
+
+      <FlatList
+        data={filteredPL}
+        keyExtractor={item => item.url}
+        renderItem={({ item }) => (
+          <Button
+            title={item.url.split("/Musica/")[1]}
+            onPress={() => setPosition(item.index)}
+          />
+        )}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff"
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  grow: {
+    flex: 1
+  }
+});
---
+++ components/Audio.js
@@ -0,0 +1,147 @@
+import React, { useState, useRef, useEffect } from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import Video from "react-native-video";
+import MusicControl from "react-native-music-control";
+
+const styles = StyleSheet.create({
+  container: {},
+  row: {
+    flexDirection: "row"
+  },
+  rowSpread: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center"
+  }
+});
+
+function formatTime(seconds) {
+  const minutes = seconds / 60;
+  return `${minutes.toFixed(0).padStart(2, "0")}:${(seconds % 60)
+    .toFixed(0)
+    .padStart(2, "0")}`;
+}
+
+export default function Audio({ path, next, prev, title }) {
+  const [progress, setProgress] = useState();
+  const [duration, setDuration] = useState(0);
+  const [speed, setSpeed] = useState(1.0);
+  const [paused, setPaused] = useState(true);
+  const [speedInput, setSpeedInput] = useState("1.0");
+
+  const playerRef = useRef();
+
+  useEffect(() => {
+    MusicControl.enableControl("play", true);
+    MusicControl.enableControl("pause", true);
+    MusicControl.enableControl("nextTrack", true);
+    MusicControl.enableControl("previousTrack", true);
+    MusicControl.enableControl("changePlaybackPosition", true);
+  }, []);
+
+  useEffect(() => {
+    if (duration && title) {
+      MusicControl.setNowPlaying({
+        title,
+        duration
+      });
+    }
+  }, [title, duration]);
+
+  function updateProgress(progress) {
+    setProgress(progress);
+    setDuration(progress.playableDuration);
+
+    MusicControl.updatePlayback({
+      // (STATE_ERROR, STATE_STOPPED, STATE_PLAYING, STATE_PAUSED, STATE_BUFFERING)
+      state: paused ? MusicControl.STATE_PAUSED : MusicControl.STATE_PLAYING,
+      speed,
+      elapsedTime: progress.currentTime
+    });
+  }
+
+  useEffect(() => {
+    MusicControl.enableBackgroundMode(true);
+    MusicControl.handleAudioInterruptions(true);
+
+    MusicControl.on("play", () => {
+      setPaused(false);
+    });
+
+    MusicControl.on("pause", () => {
+      setPaused(true);
+    });
+
+    MusicControl.on("nextTrack", () => {
+      next();
+    });
+
+    MusicControl.on("previousTrack", () => {
+      prev();
+    });
+
+    MusicControl.on("changePlaybackPosition", pos => {
+      if (playerRef.current) {
+        playerRef.current.seek(pos);
+      }
+    });
+
+    MusicControl.on("togglePlayPause", () => setPaused(p => !p));
+  }, [setPaused, next, prev]);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.rowSpread}>
+        {progress && (
+          <Text>
+            {"Time: "}
+            {formatTime(progress.currentTime)}
+            {" / "}
+            {formatTime(progress.playableDuration)}
+          </Text>
+        )}
+      </View>
+
+      <View style={styles.rowSpread}>
+        {title && (
+          <Text>
+            {"Playing: "}
+            {title}
+          </Text>
+        )}
+      </View>
+
+      <View style={styles.rowSpread}>
+        <Text>Speed:</Text>
+        <TextInput onChangeText={setSpeedInput} value={speedInput} />
+        <Button
+          onPress={() => setSpeed(parseFloat(speedInput))}
+          title={"Set"}
+        />
+      </View>
+
+      <View style={styles.rowSpread}>
+        <Button onPress={() => prev()} title={"Prev"} />
+        <Button
+          onPress={() => setPaused(p => !p)}
+          title={paused ? "Play" : "Pause"}
+        />
+        <Button onPress={() => next()} title={"Next"} />
+      </View>
+
+      <Video
+        source={path}
+        rate={speed}
+        ref={playerRef}
+        playInBackground={true}
+        playWhenInactive={true}
+        paused={paused}
+        ignoreSilentSwitch={"ignore"}
+        progressUpdateInterval={500.0}
+        onProgress={updateProgress}
+        onEnd={e => next()}
+        onError={e => console.log("VIDEO: ", e)}
+      />
+    </View>
+  );
+}
